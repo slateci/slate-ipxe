51ff832449205a44c227176bd7ae7fcb10d6a802
Emerson Ford <emersontford@gmail.com>
2019-10-28 14:43:03 -0600

    Changing the URL for iPXE

--- make-images.sh
+++ make-images.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "add iPXE submodule, reset any changes, and update"
-git submodule add git://git.ipxe.org/ipxe.git || echo "Already exists, continuing..."
+git submodule add https://git.ipxe.org/ipxe.git || echo "Already exists, continuing..."
 pushd ipxe
 git reset --hard
 git pull origin
